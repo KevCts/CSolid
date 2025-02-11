8a29f6f19d4efbd4236cbe467a9c975548a14ee3
Kevin Coetsier <kevin.coetsier@outlook.fr>
2025-02-11 21:07:18 +0100

    Correction d’une erreur dans l’algo de résolution

--- main.c
+++ main.c
@@ -11,14 +11,16 @@ Model model;
 int main(int argc, char* argv[]) {
     init_model();
 
-    add_node(0., 0, 0);
-    add_node(1., 0, 0);
+    add_node(0, 0, 0);
+    add_node(0, 1, 0);
+    add_node(1, 0, 0);
 
     add_material(210000);
 
     add_section(1);
 
-    add_bar(0, 1, 0, 0);    
+    add_bar(0, 2, 0, 0);    
+    add_bar(1, 2, 0, 0);    
 
     add_boundary(0, UX, 0);
     add_boundary(0, UY, 0);
@@ -26,19 +28,24 @@ int main(int argc, char* argv[]) {
     add_boundary(0, RX, 0);
     add_boundary(0, RY, 0);
     add_boundary(0, RZ, 0);
+    add_boundary(1, UX, 0);
     add_boundary(1, UY, 0);
     add_boundary(1, UZ, 0);
     add_boundary(1, RX, 0);
     add_boundary(1, RY, 0);
     add_boundary(1, RZ, 0);
+    add_boundary(2, UZ, 0);
+    add_boundary(2, RX, 0);
+    add_boundary(2, RY, 0);
+    add_boundary(2, RZ, 0);
 
-    add_force(1, UX, 210000);
+    add_force(2, UY, -210000);
 
     solve();
 
-    print_coomat(coomat_from_array(12, 1, model.displacements));
+    print_coomat(coomat_from_array(18, 1, model.displacements));
     printf("\n");
-    print_coomat(coomat_from_array(12, 1, model.reactions));
+    print_coomat(coomat_from_array(18, 1, model.reactions));
 
     free_model();
 
